f1cded9b121e380d125bddcb6c3564cbe2fc89dd
LivCastr0 <livcras@hotmail.com>
2023-06-13 14:26:43 -0300

    criando função leitor

--- mapeador.cpp
+++ mapeador.cpp
@@ -3,10 +3,18 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <fstream>
+#include <filesystem>
 
 using std::map;
 using std::string;
 using std::vector;
+
+namespace fs = std::filesystem;
+
+void leitor(const string& pasta){
+    
+}
 
 string normalizacao(string palavra){
     string palavranormalizada = " ";
