56c31a20ef7ac28640b9f14c4687a7802d281c24
marcusoliveira57 <marcus.oliveira57@gmail.com>
2023-06-14 18:26:35 -0300

    correcao recuperador

--- recuperador.cpp
+++ recuperador.cpp
@@ -2,6 +2,7 @@
 
     Recupera::Recupera(){
         vector<string> busca;
+        
     }
 
     void Recupera::recuperacao(map<string, map<string, int>>& mapaDe_Palavras, vector<string>& busca){
@@ -9,14 +10,14 @@
     map<string, vector<pair<string, int>>> resultados;
     
     for (auto it = busca.begin(); it != busca.end(); ++it){
-        string palavra = *it;
+        string palavra_busca = *it;
 
-        if(mapaDe_Palavras.count(palavra) > 0){
-            for (auto it = mapaDe_Palavras.at(palavra).begin(); it != mapaDe_Palavras.at(palavra).end(); ++it){
+        if(mapaDe_Palavras.count(palavra_busca) > 0){
+            for (auto it = mapaDe_Palavras.at(palavra_busca).begin(); it != mapaDe_Palavras.at(palavra_busca).end(); ++it){
                 string codDoc = it -> first;
                 int quant = it -> second;
 
-                resultados[palavra].push_back(make_pair(codDoc, quant));
+                resultados[palavra_busca].push_back(make_pair(codDoc, quant));
             }
         }
         else {
@@ -24,7 +25,7 @@
         }
 
     }
-    
+
     for(auto& mapa : resultados){
         vector<pair<string, int>>& aux = mapa.second;
 
