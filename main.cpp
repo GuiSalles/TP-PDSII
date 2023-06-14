e591c797fcd6fae93dcea537571ab4d941ead569
LivCastr0 <livcras@hotmail.com>
2023-06-14 19:34:07 -0300

    implementando a main

--- main.cpp
+++ main.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 #include <string>
+#include "mapeador.h"
+#include "normalizador.h"
+#include "recuperador.h"
+
+int main(){
+    string pasta = "caminho";
+    Mapeador mapeador;
+    mapeador.mapearPalavra(pasta);
+
+    Recupera recuperador;
+
+    string palavra;
+    vector<string> busca; 
+    recuperador.recuperacao(mapeador.mapaDe_Palavras, busca);
+
+    return 0;
+    
+
+}
