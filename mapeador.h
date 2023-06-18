0ef17bca3d8e1c6c4b7e965fc3fd0aada3732546
marcusoliveira57 <marcus.oliveira57@gmail.com>
2023-06-18 17:12:20 -0300

    consertando recuperador

--- mapeador.h
+++ mapeador.h
@@ -2,14 +2,17 @@
 #define MAPEADOR_H
 #include <map>
 #include <string>
+#include <vector>
 
 using std::map;
 using std::string;
+using std::vector;
 
 class Mapeador{
     public:
 
     void mapearPalavra(string pasta);
+    void buscarPalavra(vector<string> palavras);
 
     private:
     map<string, map<string, int>> mapaDe_Palavras;
